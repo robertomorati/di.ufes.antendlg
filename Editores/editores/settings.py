954611795e9541e8a807d8e240633a893dd663e1
robertomorati <robertomorati@gmail.com>
2014-06-20 17:03:47 -0300

    Settings

--- Editores/editores/settings.py
+++ Editores/editores/settings.py
@@ -81,7 +81,7 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, '/static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/static'
 
 # URL of the login page.
 LOGIN_URL = '/login/'
