e43d0d2e65481eade54a5c08213168b83c85946b
robertomorati <robertomorati@gmail.com>
2014-07-30 14:59:20 -0300

    Changed settings
    Change-Id: I273588ee5c5530bd429df15a1f72c9b34a4dbc3f
    Signed-off-by: robertomorati <robertomorati@gmail.com>

--- Editores/editores/settings.py
+++ Editores/editores/settings.py
@@ -85,7 +85,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles/')
+STATIC_ROOT = os.path.join(PROJECT_DIR, '/static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -96,7 +96,7 @@ LOGIN_URL = '/login/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_DIR, "static"),
+    os.path.join(PROJECT_DIR, "/staticfiles"),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -107,7 +107,7 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    #'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
