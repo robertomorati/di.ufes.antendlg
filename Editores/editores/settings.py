46d86498495ea1fb28840a00356a9071513a1405
robertomorati <robertomorati@gmail.com>
2014-07-30 13:23:26 -0300

    Change settings.
    Signed-off-by: robertomorati <robertomorati@gmail.com>

--- Editores/editores/settings.py
+++ Editores/editores/settings.py
@@ -85,7 +85,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_DIR, '/editores/static/')
+STATIC_ROOT = os.path.join(PROJECT_DIR, '/static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -99,7 +99,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_DIR, '/editores/static/'),
+    #os.path.join(PROJECT_DIR, '/editores/static/'),
 )
 
 # List of finder classes that know how to find static files in
