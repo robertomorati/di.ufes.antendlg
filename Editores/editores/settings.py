623f7c0298c37a6dcb9df59567924757bbb5b416
robertomorati <robertomorati@gmail.com>
2014-07-30 14:24:26 -0300

    Changed settings

--- Editores/editores/settings.py
+++ Editores/editores/settings.py
@@ -85,7 +85,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_DIR, '../static_files')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
