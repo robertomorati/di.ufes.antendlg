c438ef8a2ca101b05fa3d6403b213eba2baf90a7
robertomorati <robertomorati@gmail.com>
2014-08-01 12:26:01 -0300

    Changed DATADOG_APP_KEY inside settings.
    Signed-off-by: robertomorati <robertomorati@gmail.com>

--- Editores/editores/settings.py
+++ Editores/editores/settings.py
@@ -102,12 +102,12 @@ LOGIN_URL = '/autendlg/login/'
 
 #datalog config
 DATADOG_API_KEY = '429b85a4274f59adfeb8a438e3478d59'
-DATADOG_APP_KEY = '429b85a4274f59adfeb8a438e3478d59'
-DATADOG_APP_NAME = 'page.views'
+DATADOG_APP_KEY = '0b3bfdd2d4adafe4b1c65fce58be58658c87fdf9'
+#DATADOG_APP_NAME = 'page.views'
 DATADOG_APP_NAME = 'editor_enredos'
-#DATADOG_APP_NAME = 'editor_missoes'
-#DATADOG_APP_NAME = 'editor_jogadores'
-#DATADOG_APP_NAME = 'editor_movimentos'
+DATADOG_APP_NAME = 'editor_missoes'
+DATADOG_APP_NAME = 'editor_jogadores'
+DATADOG_APP_NAME = 'editor_movimentos'
 
 
 #STATSD_HOST = 'localhost'
