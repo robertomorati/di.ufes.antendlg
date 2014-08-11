77ec6ad4debd653a22f38e4c18aba99dda462304
robertomorati <robertomorati@gmail.com>
2014-08-11 10:49:17 -0300

    Chnaged the same.
    Signed-off-by: robertomorati <robertomorati@gmail.com>

--- Editores/editores/static/js/util.js
+++ Editores/editores/static/js/util.js
@@ -477,42 +477,6 @@ function createCondicaoAtiva(json_condicoes){
 	
 }
 
-function getCity(lat, lng,id_aventura) {
-
-	alert("Teste");
-    var newIdDiv = "locationAdventure"+id_aventura;
-    document.getElementById("locationAdventure").setAttribute("id", newIdDiv);
-    document.getElementById(newIdDiv).setAttribute("class", newIdDiv);
-    
-	var lat = lat.replace(',', '.');
-	var lng = lng.replace(',', '.');
-    var pos = new google.maps.LatLng(lat, lng);
-    var geocoder = new google.maps.Geocoder(); 
-    geocoder.geocode({'latLng': pos}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        if (results[1]) {
-        //find country name
-         for (var i=0; i<results[0].address_components.length; i++) {
-            for (var b=0;b<results[0].address_components[i].types.length;b++) {
-            	
-            //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
-                if (results[0].address_components[i].types[b] == "neighborhood") {
-                    //this is the object you are looking for
-                    city= results[0].address_components[i];
-                    break;
-                }
-            }
-        }
-        //city data
-         $( "."+newIdDiv ).empty().append( ""+city.short_name );
-        } else {
-        	$( "."+newIdDiv ).empty().append("Sem Localização");
-        }
-      } else {
-    	  $( "."+newIdDiv ).empty().append("Sem Localização");
-      }
-    });
-  }
 
 /*=========================================================================================================================
  |  							    		Rascunhos	Códigos com problema      						      			  |
